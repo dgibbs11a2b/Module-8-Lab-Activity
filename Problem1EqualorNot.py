b9f8df45f3fa286d730dcda1bc225c127a6b06de
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-03-11 20:22:14 -0500

    Update Problem1EqualorNot.py

--- Problem1EqualorNot.py
+++ Problem1EqualorNot.py
@@ -12,6 +12,7 @@ y = int(input("What is your second number?: "))
 #Requests input from the user
 
 if x >= y:
+#change this to:  if x == y    it doesn't matter if it is greater or not.
     print("Your first number",x, "is equal to your second number",y,)
 elif x != y:
     print("Your first number",x, "is not equal to your second number",y,)
